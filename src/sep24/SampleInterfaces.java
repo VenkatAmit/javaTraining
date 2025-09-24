3712e79e0dfcc3369a97469dbec936c0e24ac028
VenkatAmit <150881400+VenkatAmit@users.noreply.github.com>
2025-09-24 12:00:55 -0500

    Pushing the 9/23 training code

--- src/sep24/SampleInterfaces.java
+++ src/sep24/SampleInterfaces.java
@@ -3,6 +3,11 @@ package sep24;
 //Interface serves as a contract between the client and developer
 //Client --> is the interface which provides the set of abstract methods
 //Developer --> Class that implements the logics for all the abstract methods
+//Interface helps us achieve 100% abstraction till JDK8
+//By default all the methods in the interface are public in nature
+//By Default all the variables in the interface are public, static in nature
+//We can not create an objects foi
 
 public interface SampleInterfaces {
+
 }
